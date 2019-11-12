13a365ed48cd5d183e8edd785eea01fc6771495f
Guilherme Rosa <guilherme.rosa.santos@outlook.com>
2019-11-12 19:56:41 -0300

    construcao servico que chama uma api

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,19 @@
+import { Restaurant } from "./restaurant/restaurant.model";
+
+import 'rxjs/add/operator/map'
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { MEAT_API } from "app/app.api";
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class RestaurantsService {
+
+  constructor(private http: Http){}
+
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())
+  }
+
+}
