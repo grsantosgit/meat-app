dfa1b03a998617ae82fd21b7b8f00d5f9b4dd52e
Guilherme Rosa <guilherme.rosa.santos@outlook.com>
2020-03-05 09:56:20 +0000

    adicionando rotas staticas

--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -3,8 +3,10 @@ export interface Restaurant {
   id: string
   name: string
   category: string
+  about: string
   deliveryEstimate: string
   rating: number
   imagePath: string
+  hours: string
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,10 +1,12 @@
 import { Restaurant } from "./restaurant/restaurant.model";
 
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
+import { ErrorHandler} from 'app/app.error-handler'
 
 @Injectable()
 export class RestaurantsService {
@@ -14,6 +16,13 @@ export class RestaurantsService {
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
+  }
+
+  restaurantById(id: string): Observable<Restaurant>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
   }
 
 }
