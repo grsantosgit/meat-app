aca211bcebb99315478abecccf5616553bdeadc9
Guilherme Rosa <guilherme.rosa.santos@outlook.com>
2020-03-17 10:04:22 +0000

    implementig cart feature

--- src/app/restaurant-detail/shopping-cart/shopping-cart-service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart-service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from "@angular/core"
+import { CartItem } from "./cart-item.model"
+import { MenuItem } from "../menu-item/menu-item.model"
 
 @Injectable()
 export class ShoppingCartService{
 
-  items: any[]
+  items: CartItem[] = []
 
   clear(){
-
+    this.items = []
   }
 
-  addItem(item:any){
-
+  addItem(item:MenuItem){
+    let foundItem = this.items.find((mItem) => mItem.menuItem.id == item.id)
+    if(foundItem){
+      foundItem.quantity = foundItem.quantity +1;
+    }else{
+      this.items.push(new CartItem(item))
+    }
   }
 
-  removeItem(item:any){
-
+  removeItem(item:CartItem){
+    this.items.splice(this.items.indexOf(item),1)
   }
 
   total(): number{
-    return 0
+    return this.items
+        .map(item => item.value())
+        .reduce((prev, value) => prev+value, 0)
   }
 
 }
